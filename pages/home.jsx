488ec5b2e6f950b278762bc4d4746e48fbc95ece
ANKITROYCHOWDHURY54 <22p31a0504@acet.ac.in>
2024-12-10 09:39:55 +0530

    new changes added

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/header";
 import SpecialityMenu from "../components/specialityMenu";
+import Topdoctors from "../components/topdoctors";
 const Home = () =>{
     return(
         <>
            <Header /> 
            <SpecialityMenu />
+           <Topdoctors/>
         </>
     )
 }
