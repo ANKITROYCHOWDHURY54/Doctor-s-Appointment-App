e89176a43fe4ff1362c723fcddee50947fe26d47
ANKITROYCHOWDHURY54 <22p31a0504@acet.ac.in>
2025-01-14 09:59:39 +0530

    Appointment-App

--- pages/home.jsx
+++ pages/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/header";
 import SpecialityMenu from "../components/specialityMenu";
 import Topdoctors from "../components/topdoctors";
+import Banner from "../components/Banner";
 const Home = () =>{
     return(
         <>
            <Header /> 
            <SpecialityMenu />
            <Topdoctors/>
+           <Banner/>
         </>
     )
 }
